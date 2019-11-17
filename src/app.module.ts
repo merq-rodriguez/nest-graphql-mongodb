2411deef1b62222df0a53302ee7241e0d424b021
Stiven Rodriguez <judrobinx@gmail.com>
2019-11-16 23:42:26 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    ItemsModule,
+    MongooseModule.forRoot('mongodb://localhost/nestgraphql',{ useNewUrlParser: true, useUnifiedTopology: true })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/items/interfaces/item.interface.ts
@@ -0,0 +1,8 @@
+
+import { Document } from 'mongoose';
+
+export interface Item extends Document {
+    readonly title: string;
+    readonly price: number;
+    readonly description: string;
+}
